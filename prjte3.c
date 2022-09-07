b301556c966627d79ee979c47325f04a7081da3a
ayoubelmouhssine <ayoubelmouhssine01@gmail.com>
2022-09-08 00:05:16 +0200

    ajuter prjet3

---
+++ prjte3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ float mettre,mile;
+printf("entre de mettre ");
+scanf("%f",&mettre);
+mile=0.001*1.609*mettre;
+printf("%f",mile);
+
+    return 0;
+}
+
